149718569de8789943bf63106968debb627eadfb
Alejandro Ciuba <alejandrociuba@gmail.com>
2021-06-17 20:24:28 -0400

    Still working on jumping

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,15 +1,12 @@
 using UnityEngine; //Mandatory
-
+using System.Collections;
 public class PlayerMovement : MonoBehaviour //All Unity Scripts inherit from a certain group of Objects, MonoBehaviour is the most common
 {
     //Update is called once per frame
     void Update() 
     {
         AnimationControl();
-        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()) Jump();
-        if(Input.GetKey(KeyCode.Space) && jumpCounter > 0 && isJumping) VarJump();
-        if(Input.GetKeyUp(KeyCode.Space)) isJumping = false;
-        UnityEngine.Debug.Log(IsGrounded() + " " + isJumping);
+        if(IsGrounded() && Input.GetKeyDown(KeyCode.Space)) Jump();
     }
 
     //FixedUpdate is called consistently regardless of FPS (every .02 seconds per gametime)
@@ -27,13 +24,9 @@ public class PlayerMovement : MonoBehaviour //All Unity Scripts inherit from a c
     [SerializeField] private Rigidbody2D prb;
     //Player's Collider Component (Any 2D Shape)
     [SerializeField] private Collider2D pcol;
-    [SerializeField] private LayerMask jumpLayer;
-    [SerializeField] private float jumpHeight = 10f;
-    [SerializeField] private float forgive = .1f; private float forgiveCounter;
-    [SerializeField] private float jumpTime; private float jumpCounter;
-    private bool isJumping;
     [SerializeField] private float speed = 1000f;
-    [SerializeField] private float fall;
+    [SerializeField] private LayerMask jumpLayer;
+    [SerializeField] private float jump; [SerializeField] private float fall;
     //The 2 dimensional vector which tracks the players x and y movement
     private Vector2 movement;
     
@@ -46,28 +39,20 @@ public class PlayerMovement : MonoBehaviour //All Unity Scripts inherit from a c
         prb.velocity = new Vector2(movement.x * speed * Time.deltaTime, prb.velocity.y);
     }
 
-    private void Jump()
+    private void Jump() 
     {
-        isJumping = true;
-        jumpCounter = jumpTime;
-        prb.velocity = new Vector2(prb.velocity.x, 0f);
-        prb.angularVelocity = 0f;
-        prb.velocity = jumpHeight * Vector2.up;
+        prb.velocity = Vector2.up * jump;
     }
 
     private void VarJump()
     {
-        prb.velocity = Vector2.up * .75f * jumpHeight;
-        jumpCounter -= Time.deltaTime;
     }
 
-    private bool IsGrounded() {return Physics2D.BoxCast(pcol.bounds.center, pcol.bounds.size, 0f, Vector2.down, 1f, jumpLayer).collider != null;}
+    private bool IsGrounded() {return Physics2D.BoxCast(pcol.bounds.center, pcol.bounds.size, 0f, Vector2.down, .3f, jumpLayer).collider != null;}
 
     //Once the player starts coming down from a jump, make them fall faster
     private void Fall() 
     {
-        if(prb.velocity.y < 0f) 
-            prb.velocity += Vector2.up * Physics.gravity.y * (fall - 1) * Time.deltaTime;
     }
 
 //================== Animation Controls ==================
